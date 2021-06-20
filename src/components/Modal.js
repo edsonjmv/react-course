c80ffbafcc858043a2b5c9826c6ab7caa69cbcb1
Edson Machado <edsonjmv@gmail.com>
2021-06-20 10:14:06 +0200

    adding more components

---
+++ src/components/Modal.js
@@ -0,0 +1,11 @@
+function Modal() {
+  return (
+    <div className="modal">
+      <p>Are you sure?</p>
+      <button class="btn btn--alt">Cancel</button>
+      <button class="btn">Confirm</button>
+    </div>
+  );
+}
+
+export default Modal;
