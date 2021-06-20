604f5431c297e5f837fd890df951d968974efd2c
Edson Machado <edsonjmv@gmail.com>
2021-06-20 10:20:31 +0200

    add component state to handle modal display

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,12 @@
+import { useState } from 'react';
+import Modal from './Modal';
+import Backdrop from './Backdrop';
+
 function Todo(props) {
+  const [showModal, setModalIsOpen] = useState(false);
+
   function deleteHandler() {
-    console.log('clicked');
-    console.log(props.text);
+    setModalIsOpen(true);
   }
 
   return (
@@ -12,6 +17,8 @@ function Todo(props) {
           Delete
         </button>
       </div>
+      {showModal && <Modal />}
+      {showModal && <Backdrop />}
     </div>
   );
 }
